9a224822973e0a0b69494e67697171c6ded21080
EphremDemlew <ephyaabb@gmail.com>
2025-03-15 19:17:49 +0300

    👷 fix: build issue fixed

--- src/components/ui/ListCharacter.tsx
+++ src/components/ui/ListCharacter.tsx
@@ -111,7 +111,7 @@ const ListCharacter = () => {
                     className="px-6 py-2 bg-white text-black rounded-lg hover:bg-gray-200 disabled:bg-gray-400 transition-all duration-200 cursor-pointer font-medium"
                 >
                     {isFetchingNextPage
-                        ? 'Loading more...'
+                        ? (<Skeleton />)
                         : hasNextPage
                             ? 'Load More'
                             : 'No more characters'}
